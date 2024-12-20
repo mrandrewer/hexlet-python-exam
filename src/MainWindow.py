a1c2558d9c44999972f837a085d94bfccfc24416
Andrei Belskii <mrandrewer@yandex.ru>
2024-12-20 03:36:43 +0300

    Set windows size and position

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import (
 from Materials import MaterialsListView
 from controls.MaterialsWidget import MaterialsWidget
 from model.Material import Material
+from PyQt5.QtWidgets import QApplication
 
 
 class MainWindow(QMainWindow):
@@ -27,19 +28,17 @@ class MainWindow(QMainWindow):
         filter_box = QComboBox(parent=widget)
         header_layout.addWidget(filter_box)
         main_layout.addLayout(header_layout)
-
-        material = Material()
-        material.type = "Нитки"
-        material.name = "Нитки какие-то 2х2"
-        material.min_amount = 20
-        material.inventory = 100
-        material.suppliers = "Рога и копыта"
-        material.image = "picture.png"
-
-        materialWidget = MaterialsWidget(material, self)
-        main_layout.addWidget(materialWidget)
-
         materials_view = MaterialsListView(widget)
         main_layout.addWidget(materials_view)
         widget.setLayout(main_layout)
         self.setCentralWidget(widget)
+        self.setMinimumSize(700, 500)
+        self.center()
+
+    def center(self):
+        frameGm = self.frameGeometry()
+        screen = QApplication.desktop() \
+            .screenNumber(QApplication.desktop().cursor().pos())
+        centerPoint = QApplication.desktop().screenGeometry(screen).center()
+        frameGm.moveCenter(centerPoint)
+        self.move(frameGm.topLeft())
